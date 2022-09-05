fea4ed47ddcb928681a423a77fdd238601832a64
aks0208 <bazdarajla4@gmail.com>
2022-09-05 11:48:56 +0200

    fix on logout

--- components/common/ProfileItem.vue
+++ components/common/ProfileItem.vue
@@ -1,15 +1,15 @@
 <template>
-  <a href="#" class="flex-shrink-0 w-full group block">
+  <div class="flex-shrink-0 w-full group block">
     <div class="flex items-center">
       <div>
         <img class="inline-block h-9 w-9 rounded-full" src="/avatar.jpeg" alt="" />
       </div>
       <div class="ml-3">
         <p class="text-sm font-medium text-gray-700 group-hover:text-gray-900">{{$auth.user.first_name}} {{$auth.user.last_name}}</p>
-        <p @click="$auth.logout()" class="text-xs font-medium text-gray-500 group-hover:text-gray-700">Logout</p>
+        <p @click="$auth.logout();$router.push('/login')" class="text-xs cursor-pointer font-medium text-gray-500 group-hover:text-gray-700">Logout</p>
       </div>
     </div>
-  </a>
+  </div>
 
 
 </template>
